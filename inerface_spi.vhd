cfffb75757f35786b7f8fadef1706e1034ab4f1a
nicolas <ndaout@hardwaresoftware.com>
2020-05-20 00:09:17 +0200

    driver gyrocsope ...suite

--- inerface_spi.vhd
+++ inerface_spi.vhd
@@ -35,33 +35,62 @@ entity inerface_spi is
 
 port (
 		-- Users to add ports here	
-        SPICLK : in std_logic;
+		horloge : in std_logic;
+        SPICLK : out std_logic;
         SPIRESET : in std_logic;
         SPICS : out std_logic;
         MISO : in std_logic;
-        MOSI : out std_logic  );
+        MOSI : out std_logic ;
+        COMMANDE_SPI :  in std_logic_vector (15 downto 0);
+        LECTURE_SPI :  out std_logic_vector (15 downto 0);
+        RW : in std_logic;
+        MS : in std_logic     );
 end inerface_spi;
 
 architecture Behavioral of inerface_spi is
-
+signal LECTURE_spi_temp :  std_logic_vector (15 downto 0);
+signal act_clk  : std_logic;
 begin
-    process (SPICLK)
+    process (horloge)
     
         variable cpt :INTEGER :=0;  
         variable cpt_test : INTEGER :=0; 
         variable adresse_registre : INTEGER :=0; 
         variable valeur_registre : INTEGER :=0; 
+       
      begin   
     
     if (SPIRESET = '0') then
-
+          LECTURE_spi_temp<="0000000000000000";
+          act_clk<='0';
+          cpt:=0;
+    end if;
+    if rising_edge (horloge) then
+        case cpt is
+            when 0 =>
+                SPICS <='0';
+                MOSI <= RW;
+                 act_clk<='1';
+            when 1 =>
+                MOSI <= MS;
+            when 2 TO 7 =>
+                SPICS <='0';
+                MOSI <=COMMANDE_SPI(cpt-2);
+            when 8 to 15 =>
+                LECTURE_spi_temp(cpt-8)<=MISO;
+            when 15 =>
+                 SPICS <='1';
+                  act_clk<='1';
+                 LECTURE_SPI<=LECTURE_spi_temp;
+            when others =>null  ; 
+        end case;
+    end if;
+    cpt:=cpt+1;
+    if(cpt = 100) then
         cpt:=0;
-    end if;
-    if rising_edge (SPICLK) then
-     cpt:=0;
-   
-    end if;
-    
- end process;
+    end if;   
 
+end process;
+-- en mode concurrent
+SPICLK<=horloge and act_clk;
 end Behavioral;
