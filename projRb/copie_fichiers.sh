2bd2c3857c0106f05302f45ace53e631a5885ee5
NicolasDebian <nicolas2020@nicda.fr>
2020-08-20 00:30:12 +0200

    ajout copie fichier dans rep debug

--- projRb/copie_fichiers.sh
+++ projRb/copie_fichiers.sh
@@ -4,10 +4,10 @@
 echo recopie les fichiers dans le repertoire d excecution
 cp capteur_distance.calib workspace_eclipse/Robot/projRobotDebian
 cp capteur_gyroscope1.calib  workspace_eclipse/Robot/projRobotDebian
-cp config_gyroscope	workspace_eclipse/Robot/projRobotDebian/Debug
-cp moteur1.calib           workspace_eclipse/Robot/projRobotDebian/Debug
-cp moteur2.calib          workspace_eclipse/Robot/projRobotDebian/Debug
+cp config_gyroscope	workspace_eclipse/Robot/projRobotDebian
+cp moteur1.calib           workspace_eclipse/Robot/projRobotDebian
+cp moteur2.calib          workspace_eclipse/Robot/projRobotDebian
 cp simu_capteur_distance.txt  workspace_eclipse/Robot/projRobotDebian
-cp * workspace_eclipse/Robot/projRobotDebian
+//cp -r * workspace_eclipse/Robot/projRobotDebian
 echo fait
 
