edf0e6cd0bce482f3fd83414acbdf8ab20d79f83
Nicolas <nicolas2020@nicda.fr>
2021-03-20 00:01:40 +0100

    correction telecommande

--- projRb/serveur.pl
+++ projRb/serveur.pl
@@ -31,14 +31,14 @@ while(1)
 
 	# read up to 1024 characters from the connected client
 
-	$objet_commande = new Commande( "avance");
-	$objet_commande1 = new Commande( "recule");
-	$objet_commande2 = new Commande( "avantdroite");
-	$objet_commande3 = new Commande( "avantgauche");
-	$objet_commande4 = new Commande( "arrieredroite");
-	$objet_commande5 = new Commande( "arrieregauche");
-	$objet_commande6 = new Commande( "arret");
-	$objet_commande7 = new Commande( "telemetrie");
+	$objet_commande1 = new Commande( "avance");
+	$objet_commande2 = new Commande( "recule");
+	$objet_commande3 = new Commande( "avantdroite");
+	$objet_commande4 = new Commande( "avantgauche");
+	$objet_commande5 = new Commande( "arrieredroite");
+	$objet_commande6 = new Commande( "arrieregauche");
+	$objet_commande7 = new Commande( "arret");
+	$objet_commande8 = new Commande( "telemetrie");
 
 
 	
@@ -51,15 +51,18 @@ while(1)
 		my $retour4 = "";
 		my $retour5 = "";
 		my $retour6 = "";
+		my $retour7 = "";
 		$client_socket->recv($data, 1024);
 		print "received data: ->$data<-\n";
 		
-		$retour1= $objet_commande->traiteCommande($data);
-		$retour2= $objet_commande1->traiteCommande($data);
-	        $retour3= $objet_commande2->traiteCommande($data);
-		$retour4= $objet_commande3->traiteCommande($data);
-		$retour5= $objet_commande4->traiteCommande($data);
-		$retour6= $objet_commande5->traiteCommande($data);
+		$retour1= $objet_commande1->traiteCommande($data);
+		$retour2= $objet_commande2->traiteCommande($data);
+	        $retour3= $objet_commande3->traiteCommande($data);
+		$retour4= $objet_commande4->traiteCommande($data);
+		$retour5= $objet_commande5->traiteCommande($data);
+		$retour6= $objet_commande6->traiteCommande($data);
+		$retour7= $objet_commande7->traiteCommande($data);
+		#$objet_commande7->traiteCommande($data);
 		#$objet_commande7->traiteCommande($data);
 
 		
@@ -75,6 +78,7 @@ while(1)
 		$client_socket->send("avantgauche $retour4 \n");
 		$client_socket->send("arriere droite $retour5 \n");
 		$client_socket->send("arriere gauche $retour6 \n\n");
+		$client_socket->send("arret $retour7 \n\n");
 
 		#notify client that response has been sent
 		#shutdown($client_socket, 1);
